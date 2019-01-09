d26e21e03aefab02f3a8fe6005801e1726e5bdbc
odax <nr10@albion.edu>
2019-01-09 04:12:15 -0500

    added create-public-note feature

--- src/components/AddNote/AddNote.js
+++ src/components/AddNote/AddNote.js
@@ -1,79 +1,108 @@
-import React, { Component } from 'react';
-import { addNote } from '../../actions';
-import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { addNote } from "../../actions";
+import { connect } from "react-redux";
+import { Redirect, Link } from "react-router-dom";
+import "./AddNote.css";
+import Switch from "@material-ui/core/Switch";
 
 class AddNote extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: '',
-            text: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      text: "",
+      isChecked: false
+    };
+  }
+
+  submit = e => {
+    e.preventDefault();
+    const { title, text } = this.state;
+    if (title !== "" && text !== "") {
+      const newNote = { title, text, completed: false, public: this.state.isChecked };
+      this.props.addNote(newNote);
+      this.props.history.push("/"); //history.push is an easy way to update the current route
     }
+  };
 
-    
-    submit = (e) => {
-        e.preventDefault();
-        const { title, text } = this.state;
-        if (title !== '' && text !== '') {
-            const newNote = { title, text, completed: false };
-            this.props.addNote(newNote);
-            this.props.history.push('/'); //history.push is an easy way to update the current route
-        }
+  handleChange = e => {
+    this.setState({ title: e.target.value });
+  };
+
+  _handleChange = () => {
+    this.setState({ isChecked: !this.state.isChecked });
+  };
+
+  render() {
+    const { auth } = this.props;
+    const { title, text } = this.state;
+
+    if (!auth.uid) {
+      return <Redirect to="/signin" />;
     }
-
-    handleChange = (e) => {
-        this.setState({ title: e.target.value })
+    let label;
+    if (this.state.isChecked === false) {
+        label = <p>Private</p>;
+    } else {
+        label = <p>Public</p>
     }
-
-    render() {
-
-        const { auth } = this.props;
-        const { title, text } = this.state;
-
-        if (!auth.uid) {
-            return <Redirect to='/signin'/>
-          }
-
-        return (
-            <div className='page new-note'>
-                <div>
-                    <h2>New Note</h2>
-                    <div className='new-note-form'>
-                        <form onSubmit={this.submit}>
-                            <div className='input'>
-                                <input
-                                    type='text'
-                                    placeholder='Note Title'
-                                    onChange={(e) => this.setState({ title: e.target.value })}
-                                    value={title}
-                                />
-                            </div>
-                            <div className='input'>
-                                <textarea
-                                    placeholder='Note Content'
-                                    onChange={e => this.setState({ text: e.target.value })}
-                                    value={text}></textarea>
-                            </div>
-                            <div className='input'>
-                                <button>Submit</button>
-                            </div>
-                        </form>
-                        <Link to='/'>
-                        <button> Cancel </button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="page new-note">
+        <div>
+          <h2>New Note</h2>
+          <div className="new-note-form">
+            <form onSubmit={this.submit}>
+              <div className="input">
+                <input
+                  type="text"
+                  placeholder="Note Title"
+                  onChange={e => this.setState({ title: e.target.value })}
+                  value={title}
+                />
+              </div>
+              <div className="input">
+                <textarea
+                  placeholder="Note Content"
+                  onChange={e => this.setState({ text: e.target.value })}
+                  value={text}
+                />
+              </div>
+              <div className="switch-container">
+                <label>
+                  {label}
+                  <input
+                    ref="switch"
+                    checked={this.state.isChecked}
+                    onChange={this._handleChange}
+                    className="switch"
+                    type="checkbox"
+                  />
+                  <div>
+                    <div />
+                  </div>
+                </label>
+              </div>
+              <div className="input">
+                <button>Submit</button>
+              </div>
+            </form>
+            <Link to="/">
+              <button> Cancel </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth
-    }
-}
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
 
-export default connect(mapStateToProps, { addNote })(AddNote);
+export default connect(
+  mapStateToProps,
+  { addNote }
+)(AddNote);
